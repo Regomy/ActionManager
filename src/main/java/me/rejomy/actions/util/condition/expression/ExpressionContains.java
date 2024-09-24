ca3334239b4a5a5495c7f74f6d4508e433103390
Kirill <iyaargg@gmail.com>
2024-09-24 20:14:33 +0500

    Fix expression class cast error.

--- src/main/java/me/rejomy/actions/util/condition/expression/ExpressionContains.java
+++ src/main/java/me/rejomy/actions/util/condition/expression/ExpressionContains.java
@@ -2,10 +2,10 @@ package me.rejomy.actions.util.condition.expression;
 
 import me.rejomy.actions.util.interfaces.Expression;
 
-public class ExpressionContains implements Expression<String, String> {
+public class ExpressionContains implements Expression<Object, Object> {
 
     @Override
-    public boolean test(String string, String string2) {
-        return string.toLowerCase().contains(string2.toLowerCase());
+    public boolean test(Object string, Object string2) {
+        return String.valueOf(string).toLowerCase().contains(String.valueOf(string2).toLowerCase());
     }
 }
