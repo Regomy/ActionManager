a96ff80015097240c1e7ebeee88d8c90428f6f20
Kirill <iyaargg@gmail.com>
2024-10-20 20:56:05 +0500

    forget to delete debug.

--- src/main/java/me/rejomy/actions/util/condition/expression/ExpressionNotEquals.java
+++ src/main/java/me/rejomy/actions/util/condition/expression/ExpressionNotEquals.java
@@ -8,7 +8,6 @@ public class ExpressionNotEquals implements Expression<Object, Object> {
     @Override
     public boolean test(Object object, Object object2) {
         return ExpressionUtil.tryToCompare(object, object2, (objectA, objectB) -> {
-            System.out.println("a != " + objectA + " " + objectB + " result1111=" + (!objectA.equals(objectB)));
             return !objectA.equals(objectB);
         });
     }
